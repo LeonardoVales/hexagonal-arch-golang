00f93c93ab6ab2102fdbe04a1f25dd735d8e0ecf
Leonardo Vales <leonardo.vales@getrak.com.br>
2023-05-01 17:17:26 -0300

    Implementando cmd

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.7.0
 	github.com/stretchr/testify v1.8.1
 	github.com/urfave/negroni v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/LeonardoVales/arquitetura-hexagonal-go/adapters/db"
-	"github.com/LeonardoVales/arquitetura-hexagonal-go/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/LeonardoVales/arquitetura-hexagonal-go/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
